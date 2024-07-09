6ebdef16394fdfd977fba8e593c1028d5f047520
Jonas Mumm <jonasmumm@web.de>
2024-07-09 07:25:56 +0200

    better vehicle wip

--- placeholder_controller.gd
+++ placeholder_controller.gd
@@ -1,9 +1,20 @@
 extends RigidBody3D
 
 @export var force : float
+@export var rotation_speed : float
 
 func _physics_process(delta):
-	var lr = Input.get_axis("ui_left", "ui_right")
-	var up = Input.get_axis("ui_up", "ui_down")
+	var input = Vector2(Input.get_axis("control_left", "control_right"), Input.get_axis("control_up", "control_down"))
 	
-	apply_central_force(Vector3(lr,0,up) * force)
+	if input.length_squared() == 0:
+		return
+		
+	var angInput := atan2(input.y,input.x)
+	var basisBody := -global_basis.z
+	var angBody := atan2(basisBody.z,basisBody.x)
+	
+	var diff := angle_difference(angInput,angBody)
+	var addedRotation = signf(diff) * minf(absf(diff),delta * rotation_speed)
+	
+	rotate(Vector3.UP, addedRotation)
+	apply_central_force(basisBody * force)
