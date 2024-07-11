45e0bde27a931476595fa11e4d2ebbdedc811d45
Jonas Mumm <jonasmumm@web.de>
2024-07-11 10:53:59 +0200

    props placed

--- D3D/vegetation_meshes/scenes/prop.gd
+++ D3D/vegetation_meshes/scenes/prop.gd
@@ -3,6 +3,7 @@ extends StaticBody3D
 @export var randomize_y_rotation : bool
 @export var meshes : Array[Mesh]
 @export var meshInstance : MeshInstance3D
+@export var randomize_scale_range : float
 
 func _ready():
 	if randomize_y_rotation:
@@ -10,3 +11,6 @@ func _ready():
 		
 	if meshes.size() > 0:
 		meshInstance.mesh = meshes[randi_range(0,meshes.size() - 1)]
+		
+	if randomize_scale_range != 0:
+		scale *= randf_range(1-randomize_scale_range*0.5,1+randomize_scale_range*0.5)
