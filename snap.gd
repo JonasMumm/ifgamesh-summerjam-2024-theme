3888435f24770472d97e12d233722bc6d5152bbc
Jonas Mumm <jonasmumm@web.de>
2024-07-11 14:27:29 +0200

    implement polaroids + fade in animation. it's not great, but it works?

--- snap.gd
+++ snap.gd
@@ -2,6 +2,11 @@ class_name snap
 extends Node3D
 
 @export var animation_player : AnimationPlayer
+@export var texture : Texture2D
+@export var texturerect : TextureRect
+
+func _ready():
+	texturerect.texture = texture
 
 func appear():
 	visible = true
