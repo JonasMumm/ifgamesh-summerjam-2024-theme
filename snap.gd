3005dfe1246904d1dfdf1fffac3dc6ad71fb6fe0
Jonas Mumm <jonasmumm@web.de>
2024-07-18 20:57:51 +0200

    fix playing snap sound preemtively

--- snap.gd
+++ snap.gd
@@ -4,6 +4,7 @@ extends Node3D
 @export var animation_player : AnimationPlayer
 @export var texture : Texture2D
 @export var texturerect : TextureRect
+@export var audio_stram_player : AudioStreamPlayer
 
 func _ready():
 	texturerect.texture = texture
@@ -12,11 +13,11 @@ func appear():
 	visible = true
 	process_mode = PROCESS_MODE_INHERIT
 	animation_player.play("appear")
+	audio_stram_player.play()
 
 func disappear():
 	visible = false
 	process_mode = PROCESS_MODE_DISABLED
-
 
 func _on_checkpoint_entered(point, vehicle):
 	appear()
